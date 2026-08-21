96cedc4b55cb0afcaaed7c31b34f1dcb6e66e418
jaedonm <jaedon.ma@student.unimelb.edu.au>
2026-08-22 01:19:27 +1000

    Set up folder structure and SpringBoot

---
+++ src/main/java/com/gen3/recommenderagent/RecommenderAgentApplication.java
@@ -0,0 +1,13 @@
+package com.gen3.recommenderagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommenderAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommenderAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gen3/recommenderagent/RecommenderAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gen3.recommenderagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommenderAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
